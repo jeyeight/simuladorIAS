1741d92ae975bd201d0924e8ad9f41bc9b73743f
jeyeight <jvplucas@hotmail.com>
2023-12-05 21:02:23 -0300

    first commit

---
+++ header.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+//Definições
+#define size_memory 4965
+
+//Structs
+
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "header.h"
+
+int main(){
+    int memory[size_memory];
+    printf("%d", size_memory);
+}
+
