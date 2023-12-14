15f0f0118ab54e8e694e73843eebfa0d2b322716
jeyeight <jvplucas@hotmail.com>
2023-12-13 21:57:01 -0300

    Atualizações

--- header.h
+++ header.h
@@ -2,10 +2,14 @@
 #define HEADER_H
 
 //Definições
-#define size_memory 4965
+#define size_memory 4096
 #define TRUE 1
 #define FALSE 0
+#define numeros_memoria 500
 
 //Structs
+
+//Funções
+void pegarMemoria(char* memoria, FILE* fd);
 
 #endif
--- main.c
+++ main.c
@@ -5,17 +5,44 @@
 
 int main(){
     FILE* fd = fopen("entrada.txt", "r");
-    char inputLinha[100]; 
-    int countLinha = 0;
-    char* m = (char*) malloc(4965 * 5 * sizeof(char));
+    char* m = (char*) malloc(4096 * 5 * sizeof(char));
 
     if (fd == NULL) {
         perror("Erro ao abrir o arquivo");
         exit(EXIT_FAILURE);
     }
-    
+
+    pegarMemoria(m, fd);
+
     free(m);
     fclose(fd);
 
     return 0;
+}
+
+void pegarMemoria(char* memoria, FILE* fd){
+    char inputEsq[100];
+    char inputDir[100];
+    char c;
+
+    while(!feof(fd)){
+        c = fgetc(fd);
+        
+        while(c != ' '){
+            strcat(inputEsq, &c);
+            c = fgetc(fd);
+            printf("%c", c);    
+        }
+        printf("%s", inputEsq);
+        exit(1);
+        
+        c = fgetc(fd);
+
+        while(c != '\n'){
+            strcat(inputDir, &c);
+            c = fgetc(fd);
+        }
+
+        printf("%s", inputEsq);
+    }
 }
