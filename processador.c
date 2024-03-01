6bdb352e3d573c13d9b984128fa2a7bb381b4b27
jeyeight <jvplucas@hotmail.com>
2024-03-01 16:18:28 -0300

    Implementando operações ULA

--- processador.c
+++ processador.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "headers/utils.h"
 #include "headers/types.h"
+
+#define LIMITE_39_BITS 549755813888
 
 char executaULA(enum Operacoes Operacao, int Operando1, int Operando2){
     char result = 0;
@@ -14,27 +17,61 @@ char executaULA(enum Operacoes Operacao, int Operando1, int Operando2){
             break;
         case ADDModulo:
             printf("Operação de Adição com Módulo.\n");
+            int acumulador = atoi(BR.AC);
+            acumulador += abs(Operando1);
+            sprintf(BR.AC, "%d", acumulador);
             break;
         case SUB:
-            result = Operando1 + Operando2;
+            printf("%i - Peso ADD", Pesos[ADD]);
+            int acumulador = atoi(BR.AC);
+            acumulador -= Operando1;
+            sprintf(BR.AC, "%d", acumulador);
             break;
         case SUBModulo:
             printf("Operação de Subtração com Módulo.\n");
+            printf("Operação de Adição com Módulo.\n");
+            int acumulador = atoi(BR.AC);
+            acumulador -= abs(Operando1);
+            sprintf(BR.AC, "%d", acumulador);
             break;
         case MUL:
             printf("Operação de Multiplicação.\n");
+            long int mq = atoi(BR.MQ);
+            long int acumulador = atoi(BR.MQ);
+            acumulador = mq * Operando1;
+            if(acumulador > LIMITE_39_BITS){
+                
+            }
+            sprintf(BR.MQ, "%d", mq);
+            sprintf(BR.AC, "%d", acumulador);
             break;
         case DIV:
             printf("Operação de Divisão.\n");
+            int mq = atoi(BR.MQ);
+            int acumulador = atoi(BR.AC);
+            mq = (Operando1 / acumulador);
+            acumulador = (Operando1 % acumulador);
+            sprintf(BR.MQ, "%d", mq);
+            sprintf(BR.AC, "%d", acumulador);
             break;
         case LSH:
             printf("Operação de Deslocamento à Esquerda.\n");
+            int acumulador = atoi(BR.AC);
+            acumulador <<= 1;
+            sprintf(BR.AC, "%d", acumulador);
             break;
         case RSH:
             printf("Operação de Deslocamento à Direita.\n");
+            int acumulador = atoi(BR.AC);
+            acumulador >>= 1;
+            sprintf(BR.AC, "%d", acumulador);
             break;
         case STOR:
             transferirRR(BR.AC, m);
+            int posicao = Operando1 * 5;
+            for(int i = posicao; i<posicao+5; i++){
+                
+            }
             break;
         case STOREsq:
             printf("Operação de Armazenamento Quadrado.\n");
@@ -81,4 +118,3 @@ char executaULA(enum Operacoes Operacao, int Operando1, int Operando2){
     };
     return result;
 };
-
